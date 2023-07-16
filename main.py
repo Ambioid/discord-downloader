90175e396c76611258a08b092cfe4e2b47311437
YCH <57340908+Ambioid@users.noreply.github.com>
2023-07-16 23:13:31 +0100

    Fixed the channel download mode

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ with open("configuration.yaml", "r") as f: #Grab all the data from config file
     
 
 
-if config["mode"] == 1:
+if config["mode"] == 1: # Grab EVERYTHING
     download_dms()
     download_all_servers()
-elif config["mode"] == 2:
+elif config["mode"] == 2: #Grab all DMs
     download_dms()
-elif config["mode"] == 3:
+elif config["mode"] == 3: #Grab all guilds
     download_all_servers()
-elif config["mode"] == 4:
+elif config["mode"] == 4: #Grab entire guild
     download_guild(config['guildID'])
-elif config["mode"] == 5:
-    request = requests.get(url = f'''https://discord.com/api/v9/guilds/{config['guildID']}/channels''', headers= {"Authorization": config['authToken']})
-    download_channel(config['channelID'], "channel_download/", request.json()[0]["name"])
+elif config["mode"] == 5: #Grab entire channel
+    request = requests.get(url = f'''https://discord.com/api/v9/channels/{config['channelID']}''', headers= {"Authorization": config['authToken']})
+    download_channel(config['channelID'], "json_output/channel_download/", request.json()["name"])
 else:
     print("Mode must be number from 1-5. Please refer to configuration file")
 
