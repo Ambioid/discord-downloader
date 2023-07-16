8dab8d2f0c839f8e0b61af7e05c7e2833a55b974
YCH <57340908+Ambioid@users.noreply.github.com>
2023-07-16 23:35:11 +0100

    re-fixed main.py

--- main.py
+++ main.py
@@ -18,7 +18,17 @@ elif config["mode"] == 4: #Grab entire guild
     download_guild(config['guildID'])
 elif config["mode"] == 5: #Grab entire channel
     request = requests.get(url = f'''https://discord.com/api/v9/channels/{config['channelID']}''', headers= {"Authorization": config['authToken']})
-    download_channel(config['channelID'], "json_output/channel_download/", request.json()["name"])
+    print(request.json())
+
+    if request.json()["name"]: # If it has a name, use that, otherwise grab the DM recipient
+        name = request.json()["name"]
+        path = "json_output/channel_download/"
+    else:
+        name = request.json()["recipients"][0]["username"]
+        path = "json_output/!direct_messages"
+
+
+    download_channel(config['channelID'], path, name)
 else:
     print("Mode must be number from 1-5. Please refer to configuration file")
 
